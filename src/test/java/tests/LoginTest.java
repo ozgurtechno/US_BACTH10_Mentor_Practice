15027c53c4ba32e71083a598a4261039d67c9288
ozgurtechno <ozgur.technostudy@gmail.com>
2024-01-16 18:28:34 -0500

    Initial Commit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.DriverClass;
+
+public class LoginTest extends DriverClass {
+
+    @BeforeMethod
+    public void get_url(){
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+    }
+
+    @Test
+    public void login_with_valid_credentials(){
+
+        driver.findElement(By.id("btn-make-appointment")).click();
+        driver.findElement(By.id("txt-username")).sendKeys("John Doe");
+        driver.findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
+        driver.findElement(By.id("btn-login")).click();
+        String url = driver.getCurrentUrl();
+        Assert.assertEquals(url, "https://katalon-demo-cura.herokuapp.com/#appointment");
+    }
+
+
+
+}
--- src/test/java/utilities/DriverClass.java
+++ src/test/java/utilities/DriverClass.java
@@ -4,46 +4,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class DriverClass {
-    public WebDriver driver;
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+    public static Actions actions;
 
     @BeforeClass(alwaysRun = true) // Before Class doesn't work with groups. Because we are not running the class.
     // We are running some tests in the class. To make sure it runs all the time we should add alwaysRun=true
     public void createDriver(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
-        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
-
-        WebElement advanceButton = driver.findElement(By.id("details-button"));
-        advanceButton.click();
-
-        WebElement proceedLink = driver.findElement(By.id("proceed-link"));
-        proceedLink.click();
-
-        login();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
     }
 
     @AfterClass(alwaysRun = true)
     public void quitDriver(){
         driver.quit();
         closePreviousDrivers();
-    }
-
-    public void login(){
-        WebElement email = driver.findElement(By.id("input-email"));
-        email.sendKeys("knightrider@gmail.com");
-
-        WebElement password = driver.findElement(By.id("input-password"));
-        password.sendKeys("MichaelKnight1234");
-
-        WebElement loginButton = driver.findElement(By.cssSelector("input[type=\"submit\"]"));
-        loginButton.click();
     }
 
     public void closePreviousDrivers(){
