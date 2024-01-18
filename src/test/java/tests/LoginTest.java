f7ac8af4e2d0a9c670a80f569925f8e7afcec1cb
ozgurtechno <ozgur.technostudy@gmail.com>
2024-01-18 15:50:47 -0500

    Data Provider

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,13 +1,13 @@
 package tests;
 
+import net.bytebuddy.build.Plugin;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import utilities.DriverClass;
+
+import java.io.IOException;
 
 public class LoginTest extends DriverClass {
 
@@ -16,9 +16,9 @@ public class LoginTest extends DriverClass {
         driver.get("https://katalon-demo-cura.herokuapp.com/");
     }
 
-    @Test
-    @Parameters({"username", "password"})
-    public void login_with_valid_credentials(@Optional("Ozgur") String username, @Optional("Arslan") String password){
+    @Test(dataProvider = "loginData")
+//    @Parameters({"username", "password"})
+    public void login_with_valid_credentials(String username, String password){
 
         driver.findElement(By.id("btn-make-appointment")).click();
         driver.findElement(By.id("txt-username")).sendKeys(username);
@@ -28,12 +28,24 @@ public class LoginTest extends DriverClass {
 
         if (username.equalsIgnoreCase("John Doe") && password.equalsIgnoreCase("ThisIsNotAPassword")){
             Assert.assertEquals(url, "https://katalon-demo-cura.herokuapp.com/#appointment");
+            driver.findElement(By.xpath("//a[@id='menu-toggle']")).click();
+            WebElement logoutButton = driver.findElement(By.xpath("//a[.='Logout']"));
+            logoutButton.click();
         } else {
             Assert.assertEquals(url, "https://katalon-demo-cura.herokuapp.com/profile.php#login");
         }
 
     }
 
-
+    @DataProvider(name = "loginData")
+    public Object[][] credentials() {
+        Object[][] credentialsArray = {
+                {"John Doe", "ThisIsNotAPassword"},
+                {"John", "@John12"},
+                {"12John345", "John1234"},
+                {"fgdJohn", "12John4567"}
+        };
+        return credentialsArray;
+    }
 
 }
