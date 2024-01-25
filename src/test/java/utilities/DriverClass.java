411e43e60598d653779c5667bfbdefdcfd7ee590
ozgurtechno <ozgur.technostudy@gmail.com>
2024-01-25 15:01:53 -0500

    log

--- src/test/java/utilities/DriverClass.java
+++ src/test/java/utilities/DriverClass.java
@@ -1,5 +1,7 @@
 package utilities;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
@@ -10,10 +12,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -69,5 +69,29 @@ public class DriverClass {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    public static final Logger logger = LogManager.getLogger();
+    @BeforeMethod(alwaysRun = true)
+    public void logsBeforeTest(ITestResult testResult){
+        //System.out.println("Test has started");
+        // all -> trace -> debug -> info -> warn -> error -> fatal each logging level shows logs with that level and above
+
+//        logger.trace("Trace level log");
+//        logger.debug("Debug level log");
+//        logger.info("Info level log for test start");
+//        logger.warn("Warn level log");
+//        logger.error("Error level log");
+//        logger.fatal("Fatal level log");
+
+        logger.info(testResult.getMethod().getMethodName()+ " test has started");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logsAfterTest(ITestResult testResult){
+        //System.out.println("Test has ended");
+//        logger.info("Info level log for test end");
+        logger.info(testResult.getMethod().getMethodName()+" has ended with status of "+testResult.getStatus());
     }
 }
