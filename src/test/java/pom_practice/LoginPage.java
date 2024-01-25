1759f71ea6de8657413e62073b2a074935e5f314
MelikKeskiner <melikkeskiner0@gmail.com>
2024-01-25 15:26:22 -0800

    Meliks news

--- src/test/java/pom_practice/LoginPage.java
+++ src/test/java/pom_practice/LoginPage.java
@@ -1,4 +1,32 @@
 package pom_practice;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.DriverClass;
+
+import java.util.List;
+
 public class LoginPage {
+    public LoginPage(){
+        PageFactory.initElements(DriverClass.driver,this);
+    }
+    @FindBy(id = "btn-make-appointment")
+    public WebElement makeAppointment;
+
+    @FindBy(id = "txt-username")
+    public WebElement username;
+
+    @FindBy(id = "txt-password")
+    public WebElement password;
+
+    @FindBy(id = "btn-login")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//a[@id='menu-toggle']")
+    public WebElement hamburgerButton;
+
+    @FindBy(xpath = "//a[.='Logout']")
+    public WebElement logOut;
+
 }
