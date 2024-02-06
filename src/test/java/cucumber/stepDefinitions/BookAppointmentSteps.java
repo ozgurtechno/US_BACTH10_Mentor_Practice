4c6441cd7a8e8692fbc3e52749aea2ab9b20867e
ozgurtechno <ozgur.technostudy@gmail.com>
2024-02-06 15:51:39 -0500

    task completed...

--- src/test/java/cucumber/stepDefinitions/BookAppointmentSteps.java
+++ src/test/java/cucumber/stepDefinitions/BookAppointmentSteps.java
@@ -1,19 +1,29 @@
 package cucumber.stepDefinitions;
 
+import cucumber.pom.BookAppointmentPage;
 import io.cucumber.java.en.*;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import utilities.DriverClass;
 
 public class BookAppointmentSteps {
 
+    BookAppointmentPage bap = new BookAppointmentPage();
     @Given("Fill out the form")
     public void fill_out_the_form() {
-        System.out.println("done");
+        Select select = new Select(bap.facilitySelect);
+        select.getFirstSelectedOption();
+        bap.program.click();
+        bap.visitDate.sendKeys("12/05/2024");
+
     }
     @When("Click on Submit Button")
     public void click_on_submit_button() {
-        System.out.println("done");
+        bap.submitButton.click();
     }
     @Then("Appointment should be completed")
     public void appointment_should_be_completed() {
-        System.out.println("done");
+        String url = DriverClass.getDriver().getCurrentUrl();
+        Assert.assertEquals("https://katalon-demo-cura.herokuapp.com/appointment.php#summary", url);
     }
 }
