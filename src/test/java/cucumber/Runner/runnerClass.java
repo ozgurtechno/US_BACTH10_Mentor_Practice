5e29d501558fc502932b5caf97d9dff936653fbd
ozgurtechno <ozgur.technostudy@gmail.com>
2024-02-08 18:49:52 -0500

    Spark Report

--- src/test/java/cucumber/Runner/runnerClass.java
+++ src/test/java/cucumber/Runner/runnerClass.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/java/cucumber/features",
         glue = "cucumber/stepDefinitions",
-        tags = "@SmokeTest or @RegressionTest"
+        tags = "@SmokeTest or @RegressionTest",
+        plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 )
 public class runnerClass extends AbstractTestNGCucumberTests {
 }
