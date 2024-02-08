a85c2cee6788d8ba0c641840949af00ed61320c5
MelikKeskiner <melikkeskiner0@gmail.com>
2024-02-08 15:43:08 -0800

    Meliks news

--- src/test/java/cucumber/Runner/runnerClass.java
+++ src/test/java/cucumber/Runner/runnerClass.java
@@ -1,7 +1,13 @@
 package cucumber.Runner;
 
+import com.aventstack.extentreports.service.ExtentService;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import utilities.DriverClass;
 
 @CucumberOptions(
         features = "src/test/java/cucumber/features/BookAppointment.feature",
@@ -11,4 +17,24 @@ import io.cucumber.testng.CucumberOptions;
 )
 
 public class runnerClass extends AbstractTestNGCucumberTests {
+//    @BeforeClass
+//    @Parameters(value = "browser")
+//    public void setUp(@Optional("chrome") String browserName) {
+//        System.out.println("Test is starting");
+//        DriverClass.threadBrowserName.set(browserName);
+//    }
+
+    @AfterClass
+    public void terminate() {
+        System.out.println("Test is ended");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name"));
+        // Adds the info about operating system.
+        ExtentService.getInstance().setSystemInfo("Tester Name", System.getProperty("user.name"));
+        // Adds the info of the tester.
+        ExtentService.getInstance().setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        //Adds time zone info of the user
+        ExtentService.getInstance().setSystemInfo("Application Name", "Campus");
+        ExtentService.getInstance().setSystemInfo("Department Name", "QA");
+        ExtentService.getInstance().setSystemInfo("Team Name", "Team 1");
+    }
 }
