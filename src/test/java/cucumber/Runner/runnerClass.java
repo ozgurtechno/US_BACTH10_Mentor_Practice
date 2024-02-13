121fbc09477af179702b32855f90acf6913bd9a4
shaym <shaymaa2012@gmail.com>
2024-02-13 18:26:52 -0500

    My Commit

--- src/test/java/cucumber/Runner/runnerClass.java
+++ src/test/java/cucumber/Runner/runnerClass.java
@@ -2,6 +2,10 @@ package cucumber.Runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import utilities.DriverClass;
 
 @CucumberOptions(
         features = "src/test/java/cucumber/features",
@@ -10,4 +14,12 @@ import io.cucumber.testng.CucumberOptions;
         plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 )
 public class runnerClass extends AbstractTestNGCucumberTests {
+
+    @BeforeClass
+    @Parameters(value = "browser")
+    public void SetUp(@Optional(value = "chrome") String browserName) {
+        System.out.println("Test is starting");
+        DriverClass.threadBrowserName.set(browserName);
+
+    }
 }
