359f35edee07c50424472e743984ec234905df37
ozgurtechno <ozgur.technostudy@gmail.com>
2024-03-12 18:27:03 -0400

    Rest Asured

--- src/test/java/RestAssured/RestAssured.java
+++ src/test/java/RestAssured/RestAssured.java
@@ -6,17 +6,20 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class RestAssured {
+
+    private final String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjU0YWY0NmYyYjQ1MmEwNzMyYzZlNmFmMDdlMDBkZCIsInN1YiI6IjY1ZTliZjlmNWFiYTMyMDE2MzZmZjBiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O4Grq1LUD0S_haa1yTjcoB4YPtz66a2ctgBBzRsVJT4";
+
+    private final String AccountId = "21072953";
     @Test
     void sendingToken(){
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MGE1ODFjNjkxZWEyNDU5ODI3MzhhMGM4NDY4MjJhZiIsInN1YiI6IjY1ZWE1MDZmNzJjMTNlMDE4NWM1OWU5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fo_1uHwahtxqNS8lNvmxr5sCZrp54MvjPtmJP8RM3sY";
+
         given()
-                .header("Authorization","bearer",token)
+                .header("Authorization","Bearer "+TOKEN)
                 .contentType(ContentType.JSON)
                 .when()
-                .get("https://api.themoviedb.org/3/account/21072953")
-                .then();
-
-
+                .get("https://api.themoviedb.org/3/account/"+AccountId)
+                .then()
+                .log().body();
 
     }
 }
